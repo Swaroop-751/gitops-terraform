e4ba9af5478a8af7586127891a7cab76b6372bb8
Swaroop Shinde <103578627+Swaroop-751@users.noreply.github.com>
2026-05-03 10:42:47 +0530

    Add uniform_bucket_level_access to GCS bucket
    Enable uniform bucket level access for GCS bucket.

--- gcs_bucket.tf
+++ gcs_bucket.tf
@@ -2,5 +2,5 @@ resource "google_storage_bucket" "gcs1" {
     name          = "bucket-for-iac-pipeline-testing"
     storage_class = "STANDARD"
     location      = "us-central1"
-  
-} #new
+    uniform_bucket_level_access = true  
+} 
