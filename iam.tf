53cd6645d40efac23e79f90f75f1bb859da6e245
Swaroop-751 <shindeswaroop751@gmail.com>
2026-05-03 14:51:47 +0530

    Final IAM roles assigned for svc account

--- iam.tf
+++ iam.tf
@@ -1,10 +1,10 @@
-resource "google_project_iam_members" "storage_admin" {
+resource "google_project_iam_member" "storage_admin" {
   project = var.project_id
   role    = "roles/storage.admin"
   members = var.iam_users
 }
 
-resource "google_project_iam_members" "network_admin" {
+resource "google_project_iam_member" "network_admin" {
   project = var.project_id
   role    = "roles/compute.networkAdmin"
   members = var.iam_users
