498a7b7e7bd1c9f388a31b0caa606f8ac5076bc8
Swaroop-751 <shindeswaroop751@gmail.com>
2026-05-03 11:17:25 +0530

    code corrected

--- vpc.tf
+++ vpc.tf
@@ -3,24 +3,23 @@ resource "google_compute_network" "custom-vpc" {
   auto_create_subnetworks = false
   description             = "VPC for IaC pipeline testing"
   routing_mode            = "REGIONAL"
-
 }
 
 resource "google_compute_subnetwork" "sub-sg" {
-    name          = "subnet-for-iac"
-    network       = "google_compute_network.custom-vpc.id"
-    ip_cidr_range = "10.0.1.0/24"
-    region        = "us-central1"
-  
+  name          = "subnet-for-iac"
+  network       = google_compute_network.custom-vpc.id
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
 }
 
 resource "google_compute_firewall" "fw-sg" {
-    name    = "fw-for-iac"
-    network = "google_compute_network.custom-vpc.id"
-  
-    allow {
-      protocol = "tcp"
-      ports    = ["22", "80", "443"]
-    }
-  
+  name    = "fw-for-iac"
+  network = google_compute_network.custom-vpc.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
 }
